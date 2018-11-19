48385c5b9be0b35db63e1290131b9770e15371fc
Jay R. Newlin <jnewlin@industriallearningmachine.co>
2018-11-19 15:03:50 -0500

    After page 101 (Listing 3.11 - Passing props to a child component

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    item
-  </div>
+  <li>
+  	<a :href="item.url">{{ item.title }}</a>
+  	{{ item.url}}
+  </li>
 </template>
+
+<script>
+	export default {
+		props: ['item']
+	}
+</script>
