70c58ad3de829ceeffdb256b57cfa5eb3fc1f22e
Jay R. Newlin <jnewlin@industriallearningmachine.co>
2018-11-21 11:06:43 -0500

    Added Listings 5.11 and 5.12 (Testing input elements)

--- src/components/__tests__/Form.spec.js
+++ src/components/__tests__/Form.spec.js
@@ -3,8 +3,28 @@ import { shallowMount } from '@vue/test-utils'
 
 describe('Form.vue', () => {
 	test('emits form-submitted when form is submitted', () => {
-		const wrapper = shallowMount(Form)
+		const wrapper = shallowMount(Form, {
+			mocks: { axios: { post: jest.fn() } }
+		})
 		wrapper.find('button').trigger('submit')
 		expect(wrapper.emitted('form-submitted')).toHaveLength(1)
+	})
+	test('sends post request with email on submit', () => {
+		const axios = {
+			post: jest.fn()
+		}
+		const wrapper = shallowMount(Form, {
+			mocks: {
+				axios
+			}
+		})
+		const input = wrapper.find('input[type="email"]')
+		input.setValue('email@example.com')
+		wrapper.find('button').trigger('submit')
+		const url = 'http://demo7437963.mockable.io/validate'
+		const expectedData = expect.objectContaining({
+			email: 'email@example.com'
+		})
+		expect(axios.post).toHaveBeenCalledWith(url, expectedData)
 	})
 })
